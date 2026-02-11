f7ef2fac766e080a4c3b2045b49a52be2eeea0e1
moshe gertner <mg8540113@gmail.com>
2026-02-11 15:07:21 +0200

    added IP_detection_from_an_external_source

--- reader.py
+++ reader.py
@@ -1,11 +1,14 @@
 import csv
-# קריאת קובץ הלוגים - מקבלת כפרמטר את הנתיב
-def log_file_format(path):
-    with open(path, "r") as log_file:
+
+def log_file_format():
+    with open("./network_traffic.log", "r") as log_file:
         list_log = [line for line in csv.reader(log_file)]
         return list_log
 
 
 
+def IP_detection_from_an_external_source(list_log):
+    list_ip = [row[1] for row in list_log if row[1][0:2] != "10" and row[1][0:7] != "192.168"]
+    return list_ip
 
 
