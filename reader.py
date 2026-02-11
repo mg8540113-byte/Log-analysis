be174de25ae708b6a74edacbd2fdf6328287ac6a
moshe gertner <mg8540113@gmail.com>
2026-02-11 15:21:41 +0200

    added Identifying_dangerous_port

--- reader.py
+++ reader.py
@@ -1,14 +1,19 @@
 import csv
-
+# ייצוא כל הלוגים לתוך מטריצה
 def log_file_format():
     with open("./network_traffic.log", "r") as log_file:
         list_log = [line for line in csv.reader(log_file)]
         return list_log
 
 
-
+# החזרת כל כתובות ה IP ממקור חיצוני
 def IP_detection_from_an_external_source(list_log):
-    list_ip = [row[1] for row in list_log if row[1][0:2] != "10" and row[1][0:7] != "192.168"]
+    list_ip = [row[1] for row in list_log if row[1][0:3] != "10" and row[1][0:7] != "192.168"]
     return list_ip
 
+
+# בדיקת כניסות מפורטים מסוכנים, מחזיר את כל השורה, מקבל את כל המטריצה
+def Identifying_dangerous_port(list_log):
+    list_port = [row for row in list_log if row[3] in ["23", "22", "3389"]]
+    return list_port
 
